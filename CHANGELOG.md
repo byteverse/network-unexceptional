c70425aa8d854a34aae4836fa0ae5580a7436d04
Andrew Martin <andrew.thaddeus@gmail.com>
2023-07-13 12:50:53 -0400

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for network-unexceptional
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version.
---
+++ src/Network/Unexceptional/Bytes.hs
@@ -0,0 +1,49 @@
+{-# language BangPatterns #-}
+{-# language DuplicateRecordFields #-}
+{-# language PatternSynonyms #-}
+{-# language LambdaCase #-}
+{-# language NamedFieldPuns #-}
+
+module Network.Unexceptional.Bytes
+  ( sendAll
+  ) where
+
+import System.Posix.Types (Fd(Fd))
+import Foreign.C.Error (Errno)
+import Foreign.C.Types (CInt)
+import Network.Socket (Socket)
+import Data.Bytes.Types (Bytes(Bytes))
+import Foreign.C.Error.Pattern (pattern EWOULDBLOCK,pattern EAGAIN)
+import Data.Array.Byte (ByteArray)
+import GHC.Conc (threadWaitRead, threadWaitWrite)
+
+import qualified Posix.Socket as X
+import qualified Linux.Socket as X
+import qualified Data.Bytes.Types
+import qualified Network.Socket as S
+
+sendAll ::
+     Socket
+  -> Bytes
+  -> IO (Either Errno ())
+sendAll s Bytes{array,offset,length=len} = S.withFdSocket s $ \fd ->
+  -- We attempt the first send without testing if the socket is in
+  -- ready for writes. This is because it is uncommon for the transmit
+  -- buffer to already be full.
+  sendLoop (Fd fd) array offset len
+
+-- does not wait for file descriptor to be ready
+sendLoop :: Fd -> ByteArray -> Int -> Int -> IO (Either Errno ())
+sendLoop !fd !arr !off !len =
+  X.uninterruptibleSendByteArray fd arr off (fromIntegral len) (X.noSignal <> X.dontWait) >>= \case
+    Left e -> if e == EAGAIN || e == EWOULDBLOCK
+      then do
+        threadWaitWrite fd
+        sendLoop fd arr off len
+      else pure (Left e)
+    Right sentSzC ->
+      let sentSz = fromIntegral sentSzC :: Int
+       in case compare sentSz len of
+            EQ -> pure (Right ())
+            LT -> sendLoop fd arr (off + sentSz) (len - sentSz)
+            GT -> fail "Network.Unexceptional.Bytes.sendAll: send claimed to send too many bytes"
