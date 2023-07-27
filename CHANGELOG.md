b5bd08c0394dfff615aabf65182fea6549ec74da
Andrew Martin <andrew.thaddeus@gmail.com>
2023-07-27 13:18:26 -0400

    Add module for sending and receiving ByteString

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for network-unexceptional
 
+## 0.1.1.0 -- 2023-??-??
+
+* Add Network.Unexceptional.ByteString module
+
 ## 0.1.0.0 -- YYYY-mm-dd
 
 * First version.
--- src/Network/Unexceptional/ByteString.hs
+++ src/Network/Unexceptional/ByteString.hs
@@ -1,70 +1,76 @@
 {-# language BangPatterns #-}
 {-# language DuplicateRecordFields #-}
+{-# language MagicHash #-}
 {-# language PatternSynonyms #-}
 {-# language LambdaCase #-}
 {-# language NamedFieldPuns #-}
 
-module Network.Unexceptional.Bytes
+module Network.Unexceptional.ByteString
   ( send
   , receive
   ) where
 
-import System.Posix.Types (Fd(Fd))
-import Foreign.C.Error (Errno)
-import Foreign.C.Types (CInt)
-import Network.Socket (Socket)
-import Data.Bytes.Types (Bytes(Bytes),MutableBytes(MutableBytes))
-import Foreign.C.Error.Pattern (pattern EWOULDBLOCK,pattern EAGAIN)
-import Data.Array.Byte (ByteArray)
-import GHC.Conc (threadWaitRead, threadWaitWrite)
 import Control.Monad (when)
+import Data.ByteString.Internal (ByteString(BS))
+import Data.Bytes.Types (MutableBytes(MutableBytes))
+import Data.Primitive (ByteArray(ByteArray))
+import Data.Primitive.Addr (Addr(Addr),plusAddr)
+import Foreign.C.Error (Errno)
+import Foreign.C.Error.Pattern (pattern EWOULDBLOCK,pattern EAGAIN)
+import GHC.Conc (threadWaitWrite)
+import GHC.ForeignPtr (ForeignPtr(ForeignPtr),ForeignPtrContents(PlainPtr))
+import Network.Socket (Socket)
+import System.Posix.Types (Fd(Fd))
 
-import qualified Posix.Socket as X
-import qualified Linux.Socket as X
-import qualified Data.Bytes.Types
-import qualified Network.Socket as S
+import qualified Data.ByteString.Unsafe as ByteString
 import qualified Data.Primitive as PM
+import qualified GHC.Exts as Exts
+import qualified Linux.Socket as X
+import qualified Network.Socket as S
 import qualified Network.Unexceptional.MutableBytes as MB
+import qualified Posix.Socket as X
 
 -- | Send the entire byte sequence. This call POSIX @send@ in a loop
 -- until all of the bytes have been sent.
 send ::
      Socket
-  -> Bytes
+  -> ByteString
   -> IO (Either Errno ())
-send s Bytes{array,offset,length=len} = S.withFdSocket s $ \fd ->
+send s !b =
+  S.withFdSocket s $ \fd -> ByteString.unsafeUseAsCStringLen b $ \(PM.Ptr ptr,len) ->
   -- We attempt the first send without testing if the socket is in
   -- ready for writes. This is because it is uncommon for the transmit
   -- buffer to already be full.
-  sendLoop (Fd fd) array offset len
+  sendLoop (Fd fd) (Addr ptr) len
 
 -- does not wait for file descriptor to be ready
-sendLoop :: Fd -> ByteArray -> Int -> Int -> IO (Either Errno ())
-sendLoop !fd !arr !off !len =
-  X.uninterruptibleSendByteArray fd arr off (fromIntegral len) (X.noSignal <> X.dontWait) >>= \case
+sendLoop :: Fd -> Addr -> Int -> IO (Either Errno ())
+sendLoop !fd !addr !len =
+  X.uninterruptibleSend fd addr (fromIntegral len) (X.noSignal <> X.dontWait) >>= \case
     Left e -> if e == EAGAIN || e == EWOULDBLOCK
       then do
         threadWaitWrite fd
-        sendLoop fd arr off len
+        sendLoop fd addr len
       else pure (Left e)
     Right sentSzC ->
       let sentSz = fromIntegral sentSzC :: Int
        in case compare sentSz len of
             EQ -> pure (Right ())
-            LT -> sendLoop fd arr (off + sentSz) (len - sentSz)
-            GT -> fail "Network.Unexceptional.Bytes.sendAll: send claimed to send too many bytes"
+            LT -> sendLoop fd (plusAddr addr sentSz) (len - sentSz)
+            GT -> fail "Network.Unexceptional.ByteString.sendLoop: send claimed to send too many bytes"
 
 -- | If this returns zero bytes, it means that the peer has
 -- performed an orderly shutdown.
 receive :: 
      Socket
   -> Int -- ^ Maximum number of bytes to receive
-  -> IO (Either Errno Bytes)
+  -> IO (Either Errno ByteString)
 receive s n = do
-  dst <- PM.newByteArray n
+  dst <- PM.newPinnedByteArray n
   MB.receive s (MutableBytes dst 0 n) >>= \case
     Left e -> pure (Left e)
     Right m -> do
       when (m < n) (PM.shrinkMutableByteArray dst m)
-      dst' <- PM.unsafeFreezeByteArray dst 
-      pure (Right (Bytes dst' 0 m))
+      ByteArray dst# <- PM.unsafeFreezeByteArray dst 
+      pure (Right (BS (ForeignPtr (Exts.byteArrayContents# dst#) (PlainPtr (Exts.unsafeCoerce# dst#))) m))
+
