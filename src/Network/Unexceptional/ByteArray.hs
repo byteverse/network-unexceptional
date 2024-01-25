50d8b5b703398cfbcaf4bc2d7f26e192f9dadb72
Brian McKeon <bmckeon@layer3com.com>
2024-01-25 10:24:19 -0500

    Rebased for transfer of repo to byteverse. Cleanup, reformatting, removed workflows.

--- src/Network/Unexceptional/ByteArray.hs
+++ src/Network/Unexceptional/ByteArray.hs
@@ -1,38 +1,28 @@
-{-# language BangPatterns #-}
-{-# language DuplicateRecordFields #-}
-{-# language PatternSynonyms #-}
-{-# language LambdaCase #-}
-{-# language NamedFieldPuns #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Network.Unexceptional.ByteArray
   ( receiveExactly
   , receiveFromInterruptible
   ) where
 
-import Control.Exception (throwIO)
-import Control.Monad (when)
-import Data.Primitive (ByteArray)
-import Data.Bytes.Types (Bytes(Bytes),MutableBytes(MutableBytes))
-import Foreign.C.Error (Errno)
-import Foreign.C.Error.Pattern (pattern EWOULDBLOCK,pattern EAGAIN)
-import GHC.Conc (threadWaitWrite)
-import Network.Socket (Socket)
-import System.Posix.Types (Fd(Fd))
 import Control.Concurrent.STM (TVar)
+import Data.Bytes.Types (MutableBytes (MutableBytes))
+import Data.Primitive (ByteArray)
+import Foreign.C.Error (Errno)
+import Network.Socket (Socket)
 
-import qualified Network.Unexceptional.Types as Types
-import qualified Posix.Socket as X
-import qualified Linux.Socket as X
-import qualified Data.Bytes.Types
-import qualified Network.Socket as S
 import qualified Data.Primitive as PM
+import qualified Network.Socket as S
 import qualified Network.Unexceptional.MutableBytes as MB
 
 -- | Blocks until an exact number of bytes has been received.
 receiveExactly ::
-     Socket
-  -> Int -- ^ Exact number of bytes to receive, must be greater than zero
-  -> IO (Either Errno ByteArray)
+  Socket ->
+  -- | Exact number of bytes to receive, must be greater than zero
+  Int ->
+  IO (Either Errno ByteArray)
 receiveExactly !s !n = do
   dst <- PM.newByteArray n
   MB.receiveExactly s (MutableBytes dst 0 n) >>= \case
@@ -42,15 +32,16 @@ receiveExactly !s !n = do
       pure (Right dst')
 
 receiveFromInterruptible ::
-     TVar Bool
-  -> Socket
-  -> Int -- ^ Maximum number of bytes to receive.
-  -> IO (Either Errno (ByteArray, S.SockAddr))
+  TVar Bool ->
+  Socket ->
+  -- | Maximum number of bytes to receive.
+  Int ->
+  IO (Either Errno (ByteArray, S.SockAddr))
 receiveFromInterruptible !intr s !n = do
   dst <- PM.newByteArray n
   MB.receiveFromInterruptible intr s (MutableBytes dst 0 n) >>= \case
     Left err -> pure (Left err)
-    Right (sz,sockAddr) -> do
+    Right (sz, sockAddr) -> do
       PM.shrinkMutableByteArray dst sz
       dst' <- PM.unsafeFreezeByteArray dst
       pure (Right (dst', sockAddr))
--- src/Network/Unexceptional/Chunks.hs
+++ src/Network/Unexceptional/Chunks.hs
@@ -1,33 +1,31 @@
-{-# language BangPatterns #-}
-{-# language DuplicateRecordFields #-}
-{-# language PatternSynonyms #-}
-{-# language LambdaCase #-}
-{-# language NamedFieldPuns #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 
 module Network.Unexceptional.Chunks
   ( send
   , sendInterruptible
   ) where
 
+import Control.Concurrent.STM (TVar)
+import Data.Bytes.Chunks (Chunks)
 import Foreign.C.Error (Errno)
 import Network.Socket (Socket)
-import Data.Bytes.Chunks (Chunks)
-import Control.Concurrent.STM (TVar)
 
 import qualified Data.Bytes.Chunks as Chunks
 import qualified Network.Unexceptional.Bytes as NB
 
--- | Send the entire byte sequence. This call POSIX @send@ in a loop
--- until all of the bytes have been sent.
+{- | Send the entire byte sequence. This call POSIX @send@ in a loop
+until all of the bytes have been sent.
+-}
 send ::
-     Socket
-  -> Chunks
-  -> IO (Either Errno ())
+  Socket ->
+  Chunks ->
+  IO (Either Errno ())
 send !s cs = NB.send s (Chunks.concat cs)
 
 sendInterruptible ::
-     TVar Bool
-  -> Socket
-  -> Chunks
-  -> IO (Either Errno ())
+  TVar Bool ->
+  Socket ->
+  Chunks ->
+  IO (Either Errno ())
 sendInterruptible !interrupt !s cs = NB.sendInterruptible interrupt s (Chunks.concat cs)
